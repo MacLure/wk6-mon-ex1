58c61805bc9d17a191eab2082295dcc7902fedc4
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-12-10 13:54:52 -0500

    refactored the assignment

--- wk6-mon-ex1.rb
+++ wk6-mon-ex1.rb
@@ -1,18 +1,14 @@
 numbers = {}
-counter = 1
 
-50.times do
-    numbers[counter] = counter 
-    counter += 1
-end
-
-numbers.each do |key, value|
-    if key % 7 == 0 && key % 2 == 0
-        numbers[key] = key * 2
-    elsif key % 7 == 0
-        numbers[key] = key - 1
-    elsif key % 2 == 0
-        numbers[key] = key + 1
+(1..50).each do |i|
+    if i % 7 == 0 && i % 2 == 0
+        numbers[i] = i * 2
+    elsif i % 7 == 0
+        numbers[i] = i - 1
+    elsif i % 2 == 0
+        numbers[i] = i + 1
+    else
+        numbers[i] = i
     end
 end
 
